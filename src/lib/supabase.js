6e9cf0bf10a2c54a57f5f622cb61e170ecb5c93b
Guilherme Augusto <guilhermeoliveira.ov@gmail.com>
2026-06-08 00:24:56 +0000

    Deploy GitHub Pages

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  'https://zdalfvbufdqzevsketid.supabase.co',
-  'sb_publishable_xNzmaKKWPe9LwVUUsYBgkQ_-QRSIQku'
-);
--- src/services/index.js
+++
@@ -1,6 +0,0 @@
-export * from './db.js';
-export * from './ui.js';
-export * from './auth.js';
-export * from './sync.js';
-export * from './comunicacao.js';
-export * from './pdf.js';
